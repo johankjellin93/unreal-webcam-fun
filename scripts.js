db86310e277a29a228e05cafbebed3e302359608
Unknown <johankjellin93@gmail.com>
2018-04-10 23:56:01 +0200

    added effect functions

--- scripts.js
+++ scripts.js
@@ -42,6 +42,50 @@ function getVideo() {
     strip.insertBefore(link, strip.firsChild);
   }
 
+  function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i + 0] = pixels.data[i + 0] + 200;
+      pixels.data[i + 1] = pixels.data[i + 1] - 50;
+      pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+    }
+    return pixels;
+  }
+  
+  function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i - 150] = pixels.data[i + 0];
+      pixels.data[i + 500] = pixels.data[i + 1];
+      pixels.data[i - 550] = pixels.data[i + 2];
+    }
+    return pixels;
+  }
+  
+  function greenScreen(pixels) {
+    const levels = {};
+  
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+  
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+      red = pixels.data[i + 0];
+      green = pixels.data[i + 1];
+      blue = pixels.data[i + 2];
+      alpha = pixels.data[i + 3];
+  
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        pixels.data[i + 3] = 0;
+      }
+    }
+  
+    return pixels;
+  }
+
   getVideo();
 
   video.addEventListener('canplay', paintToCanvas);
