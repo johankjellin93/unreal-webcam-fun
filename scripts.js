c38dadb38ac44126a897bbcfd314c68a85745866
Unknown <johankjellin93@gmail.com>
2018-04-10 23:54:35 +0200

    added takephoto function

--- scripts.js
+++ scripts.js
@@ -30,4 +30,18 @@ function getVideo() {
     }, 16);
   }
 
+  function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+  
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    strip.insertBefore(link, strip.firsChild);
+  }
+
   getVideo();
+
+  video.addEventListener('canplay', paintToCanvas);
