9f4bdba211dd8e04401cc7775d95724b94f98e1a
Unknown <johankjellin93@gmail.com>
2018-04-10 23:47:48 +0200

    added paintcanvas function

--- scripts.js
+++ scripts.js
@@ -12,6 +12,22 @@ function getVideo() {
         video.play();
       })
       .catch(err => {
-        console.error(`o no error`);
+        console.error(`o no error`, err);
       });
   }
+
+  function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+  
+    return setInterval(() => {
+      ctx.drawImage(video, 0, 0, width, height);
+      let pixels = ctx.getImageData(0, 0, width, height);
+      pixels = rgbSplit(pixels);
+      ctx.putImageData(pixels, 0, 0);
+    }, 16);
+  }
+
+  getVideo();
